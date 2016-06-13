4012612589e0ad1c4be586e701c64ef55af9d850
tiger-greyhawk <tiger.greyhawk@gmail.com>
2016-06-13 17:59:37 +0300

    return

--- SHT/MainForm.Designer.cs
+++ SHT/MainForm.Designer.cs
@@ -30,9 +30,14 @@
         {
             this.components = new System.ComponentModel.Container();
             this.targetTime = new System.Windows.Forms.DateTimePicker();
+            this.armyWavesListView = new System.Windows.Forms.ListView();
+            this.id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.multiplier = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timeLeft = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.timeCalculationTimer = new System.Windows.Forms.Timer(this.components);
             this.attackTimeLabel = new System.Windows.Forms.Label();
-            this.armyWavesFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
             this.SuspendLayout();
             // 
             // targetTime
@@ -43,6 +48,45 @@
             this.targetTime.Name = "targetTime";
             this.targetTime.Size = new System.Drawing.Size(129, 20);
             this.targetTime.TabIndex = 1;
+            // 
+            // armyWavesListView
+            // 
+            this.armyWavesListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.armyWavesListView.CausesValidation = false;
+            this.armyWavesListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.id,
+            this.time,
+            this.multiplier,
+            this.name,
+            this.timeLeft});
+            this.armyWavesListView.Location = new System.Drawing.Point(12, 38);
+            this.armyWavesListView.Name = "armyWavesListView";
+            this.armyWavesListView.Size = new System.Drawing.Size(778, 423);
+            this.armyWavesListView.TabIndex = 2;
+            this.armyWavesListView.UseCompatibleStateImageBehavior = false;
+            this.armyWavesListView.View = System.Windows.Forms.View.Details;
+            // 
+            // id
+            // 
+            this.id.Text = "Id";
+            // 
+            // time
+            // 
+            this.time.Text = "Time";
+            // 
+            // multiplier
+            // 
+            this.multiplier.Text = "multiplier";
+            // 
+            // name
+            // 
+            this.name.Text = "name";
+            // 
+            // timeLeft
+            // 
+            this.timeLeft.Text = "Time Left";
             // 
             // timeCalculationTimer
             // 
@@ -58,24 +102,13 @@
             this.attackTimeLabel.TabIndex = 3;
             this.attackTimeLabel.Text = "Attack Time";
             // 
-            // armyWavesFlowLayoutPanel
-            // 
-            this.armyWavesFlowLayoutPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.armyWavesFlowLayoutPanel.AutoScroll = true;
-            this.armyWavesFlowLayoutPanel.Location = new System.Drawing.Point(12, 38);
-            this.armyWavesFlowLayoutPanel.Name = "armyWavesFlowLayoutPanel";
-            this.armyWavesFlowLayoutPanel.Size = new System.Drawing.Size(778, 423);
-            this.armyWavesFlowLayoutPanel.TabIndex = 4;
-            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(802, 473);
-            this.Controls.Add(this.armyWavesFlowLayoutPanel);
             this.Controls.Add(this.attackTimeLabel);
+            this.Controls.Add(this.armyWavesListView);
             this.Controls.Add(this.targetTime);
             this.Name = "MainForm";
             this.Text = "Form1";
@@ -87,9 +120,14 @@
 
         #endregion
         private System.Windows.Forms.DateTimePicker targetTime;
+        private System.Windows.Forms.ListView armyWavesListView;
+        private System.Windows.Forms.ColumnHeader id;
+        private System.Windows.Forms.ColumnHeader time;
+        private System.Windows.Forms.ColumnHeader multiplier;
+        private System.Windows.Forms.ColumnHeader name;
+        private System.Windows.Forms.ColumnHeader timeLeft;
         private System.Windows.Forms.Timer timeCalculationTimer;
         private System.Windows.Forms.Label attackTimeLabel;
-        private System.Windows.Forms.FlowLayoutPanel armyWavesFlowLayoutPanel;
     }
 }
 
--- SHT/Model/ArmyWave.cs
+++ SHT/Model/ArmyWave.cs
@@ -22,22 +22,9 @@ namespace SHT.Dto
         public int GetMultiplier() { return multiplier; }
         public string GetName() { return name; }
 
-        public string Id
-        {
-            get { return id.ToString(); }
-            set { id = Convert.ToInt32(value); }
-        }
-        public string TravelTime
-        {
-            get { return new TimeSpan(travelTime * 10000).ToString(@"dd\.hh\:mm\:ss"); }
-            set
-            {
-                TimeSpan result;
-                TimeSpan.TryParseExact(value, @"dd\.hh\:mm\:ss", null, out result);
-                travelTime = result.Ticks / 10000;
-            }
-        }
-        public string Multiplier { get { return multiplier.ToString(); } set { multiplier = Convert.ToInt32(value); } }
-        public string Name { get { return name; } set { name = value; } }
+        public string Id { get { return id.ToString(); } }
+        public string TravelTime { get { return new TimeSpan(travelTime * 10000).ToString(); } }
+        public string Multiplier { get { return multiplier.ToString(); } }
+        public string Name { get { return name; } }
     }
 }
